3dde562654540042839085dcb6df134a21d5a95e
David <sapak01@student.vspj.cz>
2020-10-20 21:21:53 +0200

    no message

---
+++ Informacni_system/Informacni_system/index.aspx.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <automaticky generován>
+//     Tento kód vygeneroval nástroj.
+//
+//     Změny tohoto souboru mohou způsobit nesprávné chování a budou ztraceny, jestliže
+//     kód je znovu vygenerován. 
+// </automaticky generován>
+//------------------------------------------------------------------------------
+
+namespace Informacni_system
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// ovládací prvek form1.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
