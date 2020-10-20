ef92704e0728bae46d2749dea72000a4ad2f41bc
David <sapak01@student.vspj.cz>
2020-10-20 21:37:57 +0200

    no message

---
+++ Informacni_system/Informacni_system/predloha.Master.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <automaticky generován>
+//     Tento kód vygeneroval nástroj.
+//
+//     Změny tohoto souboru mohou způsobit nesprávné chování a budou ztraceny, jestliže
+//     kód je znovu vygenerován. 
+// </automaticky generován>
+//------------------------------------------------------------------------------
+
+namespace Informacni_system
+{
+
+
+    public partial class predloha
+    {
+
+        /// <summary>
+        /// ovládací prvek head.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// ovládací prvek form1.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ovládací prvek ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}
