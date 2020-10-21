787892a4535d7717703f273eb3bed3ab2f1bc845
David <sapak01@student.vspj.cz>
2020-10-21 13:05:54 +0200

    no message

---
+++ Informacni_system/Informacni_system/zadani_clanku.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Informacni_system
+{
+    public partial class zadani_clanku : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void odeslat_Click(object sender, EventArgs e)
+        {
+            Response.Write(autor.Text);
+            Response.Write(clanek.Text);
+            Response.Write(texteditor.Text);
+        }
+    }
+}
---
+++ Informacni_system/Informacni_system/zadani_clanku.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <automaticky generován>
+//     Tento kód vygeneroval nástroj.
+//
+//     Změny tohoto souboru mohou způsobit nesprávné chování a budou ztraceny, jestliže
+//     kód je znovu vygenerován. 
+// </automaticky generován>
+//------------------------------------------------------------------------------
+
+namespace Informacni_system
+{
+
+
+    public partial class zadani_clanku
+    {
+
+        /// <summary>
+        /// ovládací prvek clanek.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox clanek;
+
+        /// <summary>
+        /// ovládací prvek autor.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox autor;
+
+        /// <summary>
+        /// ovládací prvek texteditor.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::FreeTextBoxControls.FreeTextBox texteditor;
+
+        /// <summary>
+        /// ovládací prvek odeslat.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button odeslat;
+    }
+}
