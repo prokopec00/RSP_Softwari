0b9337ced82d590c84cd6007f7ab1aaa01bb7a7e
klobou01 <klobou01@student.vspj.cz>
2020-11-08 23:08:16 +0100

    zadani recenze formular

---
+++ Informacni_system/Informacni_system/zadani_recenze.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Informacni_system
+{
+    public partial class zadani_recenze : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            
+
+
+        }
+
+        protected void odeslat_Click(object sender, EventArgs e)
+        {
+            global_template dbSaver= new global_template();
+          
+            dbSaver.DB_ExecuteNonQuery("INSERT INTO `tbl_review` ( `review_title`, `rating`, `review_text`, `id_article`, `id_reviewer`)" +
+                " VALUES('" + review_title.Text + "', '" + rating.Text + "', '" + texteditor.Text + "', '" + id_article.Text + "', '1')");
+           
+            //TODO ID REVIEWER
+        }
+        
+
+        //id_review 	review_title 	rating 	review_text 	id_article 	id_reviewer 
+        protected void rating_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
