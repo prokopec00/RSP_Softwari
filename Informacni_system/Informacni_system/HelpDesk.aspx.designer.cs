c42d84d8837fb5749fcf1acd20430a49247aee26
DESKTOP-JGMBLHE\Kuba <jakubrizek@email.cz>
2020-11-10 20:57:54 +0100

    Nahrání helpdesku

---
+++ Informacni_system/Informacni_system/HelpDesk.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <automaticky generován>
+//     Tento kód vygeneroval nástroj.
+//
+//     Změny tohoto souboru mohou způsobit nesprávné chování a budou ztraceny, jestliže
+//     kód je znovu vygenerován. 
+// </automaticky generován>
+//------------------------------------------------------------------------------
+
+namespace Informacni_system
+{
+
+
+    public partial class HelpDesk
+    {
+
+        /// <summary>
+        /// ovládací prvek id_name.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox id_name;
+
+        /// <summary>
+        /// ovládací prvek id_email.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox id_email;
+
+        /// <summary>
+        /// ovládací prvek id_surname.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox id_surname;
+
+        /// <summary>
+        /// ovládací prvek id_text.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox id_text;
+
+        /// <summary>
+        /// ovládací prvek odeslat.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button odeslat;
+
+        /// <summary>
+        /// ovládací prvek alert.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox alert;
+    }
+}
