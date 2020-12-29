2b8453da23be87e8dbbc1f45f6f7ca0ba58a127e
David <sapak01@student.vspj.cz>
2020-12-29 14:13:49 +0100

    Auto stash before merge of "fixes_articles" and "origin/fixes_articles"

--- Informacni_system/Informacni_system/HelpDesk.aspx.cs
+++ Informacni_system/Informacni_system/HelpDesk.aspx.cs
@@ -9,20 +9,21 @@ namespace Informacni_system
 {
     public partial class HelpDesk : System.Web.UI.Page
     {
-        //protected void Page_Load(object sender, EventArgs e)
-        //{
-        //    id_name.Text = "";
-        //    id_email.Text = "";
-        //    id_surname.Text = "";
-        //    id_text.Text = "";
-        //    
-        //}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //    id_name.Text = "";
+            //    id_email.Text = "";
+            //    id_surname.Text = "";
+            //    id_text.Text = "";
+            //  
+            alert.Text = "Ulozeno";
+        }
 
         protected void odeslat_Click(object sender, EventArgs e)
         {
             global_template gt = new global_template();
             gt.DB_ExecuteNonQuery("INSERT INTO `helpdesk` (`name`, `surname`, `email`, `text`)" + "VALUES ('" + id_name.Text + "','" + id_email.Text + "','" + id_surname.Text + "','" + id_text.Text + "')");
-            alert.Text = "Ulozeno";
+           
             
             id_name.Text = null;
             id_email.Text = null;
